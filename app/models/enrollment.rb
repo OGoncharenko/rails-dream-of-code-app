89fc26f06eb008b6ba2aee87d69485ec541c5e95
OGoncharenko <olyas.goncharenko@gmail.com>
2025-06-14 16:45:33 -0700

    Week 5: Add methods and tests to Course and Enrollment models

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,10 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student
   has_many :mentor_enrollment_assignments
+
+  def is_past_application_deadline
+    deadline = course.trimester.application_deadline
+    return false unless deadline
+    created_at > deadline
+  end
 end
